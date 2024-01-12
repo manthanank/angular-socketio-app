efaf872f8204ddb84ac1fce090af3546337f8314
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-01-12 23:41:01 +0530

    feat: added project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-socketio'`, () => {
+  it(`should have as title 'angular-socketio-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-socketio');
+    expect(app.title).toEqual('angular-socketio-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-socketio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-socketio-app app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-socketio';
+export class AppComponent implements OnInit {
+  private socket: any;
+
+  ngOnInit() {
+    this.socket = io('http://localhost:3000');
+
+    this.socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+  }
 }
